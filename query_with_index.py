cbe250c217d53d4f9ab2eb7201b726b902938ea5
akmiller01 <alex.k.miller@gmail.com>
2023-04-05 12:54:19 -0400

    Improve prompt for GPT4all

--- query_with_index.py
+++ query_with_index.py
@@ -1,5 +1,5 @@
 from langchain.llms import LlamaCpp
-from llama_index import LLMPredictor, ServiceContext, GPTSimpleVectorIndex, LangchainEmbedding
+from llama_index import QuestionAnswerPrompt, LLMPredictor, ServiceContext, GPTSimpleVectorIndex, LangchainEmbedding
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 
 embed_model = LangchainEmbedding(HuggingFaceEmbeddings())
@@ -9,8 +9,19 @@ service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, embe
 index = GPTSimpleVectorIndex.load_from_disk('index.json', service_context=service_context)
 
 query_str = "What factors have primarily caused driven changes in volumes of ODA in the last 10 years?"
+QA_PROMPT_TMPL = (
+    """
+    Context:
+    ---------------------
+    {context_str}
+    ---------------------
+    Question: {query_str}
+    Answer:
+    """
+)
+QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
 
-response = index.query(query_str, mode="embedding")
-print(str(response))
+response = index.query(query_str, text_qa_template=QA_PROMPT)
+print(response)
 print(response.get_formatted_sources())
 
